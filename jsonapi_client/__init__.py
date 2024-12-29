4953be21f44d379f395639b1e0ccbe160ffc3e7f
Felipe Batista da Silva <felipe@batistadasilva.com>
2024-12-29 11:15:36 +0100

    feat: singleton resources

--- jsonapi_client/__init__.py
+++ jsonapi_client/__init__.py
@@ -1,4 +1,4 @@
-from .collection import JsonAPICollection, T
+from .collection import JsonAPICollection, JsonAPISingleton, T
 from .query import JsonAPIFilterValue, JsonAPIQuery, JsonAPISortValue, JsonAPIIncludeValue
 
 VERSION = "0.0.3"
