02b8947412a3ebfad3ed95179399fa353ab3f315
t4tianasc <tatiana.sanchez@hostnfly.com>
2025-02-20 15:02:47 +0100

    feat: Override equality comparison in JsonAPIResourceSchema and add tests

--- jsonapi_client/schema.py
+++ jsonapi_client/schema.py
@@ -1,12 +1,18 @@
 from dataclasses import dataclass
+from typing import Any
 
 from dataclasses_json import dataclass_json
 
 
 @dataclass_json
+@dataclass
 class JsonAPIResourceSchema:
-    pass
+    id: str
 
+    def __eq__(self, other: Any) -> bool:
+        if isinstance(other, JsonAPIResourceSchema) and self.__class__ == other.__class__:
+            return self.id == other.id
+        return False
 
 @dataclass_json
 @dataclass
